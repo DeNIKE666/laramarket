8b1812c02ade11c0105680e37de6a4e571221154
next <nexxxt.07@gmail.com>
2020-08-19 00:05:10 +0300

    product attribute edit and price

---
+++ database/migrations/2020_08_18_210148_edit_price_product2.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class EditPriceProduct2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function(Blueprint $table){
+            $table->dropColumn('old_price');
+            $table->dropColumn('price');
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->dropColumn('cost');
+        });
+
+        Schema::table('products', function(Blueprint $table){
+            $table->decimal('old_price', 8, 2)->nullable();
+            $table->decimal('price', 8, 2)->default(0);
+        });
+
+        Schema::table('orders', function(Blueprint $table){
+            $table->decimal('cost', 8, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
